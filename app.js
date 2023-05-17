050faa1e7d7c8eb4f0a4e3c283ac9649fa7b1d28
Pedrinnnnn <maite.emanuely.2013@gmail.com>
2023-05-17 15:09:39 -0300

    a

---
+++ app.js
@@ -0,0 +1,30 @@
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/';
+
+let i =0;
+while (i < 151) {
+
+    //Criando um elemento <div>
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon'); //Adicionando a class pokemon ao div. Ex.: <div class = 'pokemon'></div>
+                                      //A classe pokemon está definida no arquivo app.css
+
+    //Criando um elemento <span>
+    const rotulo = document.createElement('span');
+    rotulo.innerText = (i+1); //Colocando o número do pokemon ao no texto do <span> criado.
+    rotulo.classList.add('numero')
+
+    //Criando um elemento <img>
+    const novaImg = document.createElement('img');
+    novaImg.src = baseURL+(i+1)+".gif"; //Atribuindo o endereço e o nome do arquivo de imagem no atributo src do <img> criado.
+
+    //Adicionando a imagem e o rótulo ao <div> criado
+    pokemon.appendChild(novaImg);
+    pokemon.appendChild(rotulo);
+    document.getElementById('card').appendChild(pokemon)
+
+    //Adicionando o <div> à página
+    // document.body.appendChild(pokemon);
+    
+    i++;
+}
+
